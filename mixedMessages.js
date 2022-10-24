82e4f890d0c576a2e460051eaf70cd53428e03a8
cdrosario98 <collindelrosario98@gmail.com>
2022-10-25 09:21:53 +1100

    first commit

---
+++ mixedMessages.js
@@ -0,0 +1,12 @@
+//Mixed Messages Project is a an Ad-Lib Game where the computer will randomly choose inputs for the nouns, verbs, adverbs, adjectives
+
+let nouns = [];
+let verbs = [];
+let adverbs = [];
+let adjectives = [];
+
+randomNoun = nouns[Math.floor(Math.random() * nouns.length)]
+
+let word = 'hello'
+
+console.log(word.length)
